6120a1b2e34b629a8c06550e71745bd92bef3fe3
GORDON BITTNER <1007806253@wce111-16.local>
2019-01-24 19:44:19 -0800

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by GORDON BITTNER on 1/24/19.
+//  Copyright © 2019 GORDON BITTNER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
